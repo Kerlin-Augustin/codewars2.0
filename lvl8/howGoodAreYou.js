d2669b1cb07dcd6596ab5ec9ed61f66a77520c75
Kerlin Augustin <kerlin.augustin1@gmail.com>
2023-09-03 13:09:12 -0400

    how good are you really

---
+++ lvl8/howGoodAreYou.js
@@ -0,0 +1,12 @@
+function betterThanAverage(classPoints, yourPoints) {
+  let sum = 0
+  for(let i = 0; i < classPoints.length; i++){
+    sum += classPoints[i]
+  }
+  sum = sum/classPoints.length
+  if(sum > yourPoints){
+    return false
+  } else {
+    return true
+  }
+}
