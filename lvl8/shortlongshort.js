60841d83cea6f88c96d6370667a8d6d84212ffef
Kerlin Augustin <kerlin.augustin1@gmail.com>
2023-09-14 21:24:10 -0400

    short long short

---
+++ lvl8/shortlongshort.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/50654ddff44f800200000007/train/javascript
+
+function solution(a, b){
+  if(a.length < b.length){
+    return `${a}${b}${a}`
+  } else if(a.length > b.length){
+    return `${b}${a}${b}`
+  }
+}
