7cf5f3560bd5d85d22379d3eb0597bbe60ef15c9
Kerlin Augustin <kerlin.augustin1@gmail.com>
2023-10-04 01:06:36 -0400

    switch it up

---
+++ lvl8/switchItUp.js
@@ -0,0 +1,25 @@
+// https://www.codewars.com/kata/5808dcb8f0ed42ae34000031/train/javascript
+
+function switchItUp(number){
+  if (number === 0){
+    return 'Zero'
+  } else if(number === 1){
+    return 'One'
+  } else if(number === 2){
+    return 'Two'
+  } else if(number === 3){
+    return 'Three'
+  }else if(number === 4){
+    return 'Four'
+  }else if(number === 5){
+    return 'Five'
+  }else if(number === 6){
+    return 'Six'
+  }else if(number === 7){
+    return 'Seven'
+  }else if(number === 8){
+    return 'Eight'
+  }else if(number === 9){
+    return 'Nine'
+  }
+}
