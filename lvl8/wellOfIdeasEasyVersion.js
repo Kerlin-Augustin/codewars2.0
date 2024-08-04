ab0f9d113af7fb8eb554573af4efa4bf07565c33
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-08-04 12:58:43 -0400

    well of ideas easy version

---
+++ lvl8/wellOfIdeasEasyVersion.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/57f222ce69e09c3630000212/train/javascript
+
+function well(x){
+    let count = 0
+    for(let val of x){
+      if(val === 'good'){
+        count++
+      }
+    }
+    if(count === 0){
+      return 'Fail!'
+    } else if(count > 2){
+      return 'I smell a series!'
+    } else if(count <= 2) {
+      return 'Publish!'
+    }
+  }
