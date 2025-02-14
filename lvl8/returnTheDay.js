2b173c2b825663ab2177c9480f8bb14705816c31
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-02-14 14:40:27 -0500

    return the day

---
+++ lvl8/returnTheDay.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/59dd3ccdded72fc78b000b25/train/javascript
+
+function whatday(num) { 
+  if(num === 1) return "Sunday"
+  if(num === 2) return "Monday"
+  if(num === 3) return "Tuesday"
+  if(num === 4) return "Wednesday"
+  if(num === 5) return "Thursday"
+  if(num === 6) return "Friday"
+  if(num === 7) return "Saturday"
+   return "Wrong, please enter a number between 1 and 7"
+ }
