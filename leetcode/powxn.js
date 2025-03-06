d520937e4bd52404cf464536a75cf8e7ae5fc58f
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-03-05 22:06:48 -0500

    pow xn

---
+++ leetcode/powxn.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/powx-n/description/
+
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+  return Math.pow(x,n)
+};
