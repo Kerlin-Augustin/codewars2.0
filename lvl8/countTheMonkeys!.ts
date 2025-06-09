224a983f16b1db83cccec0c9c95fbfc76eca6064
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-06-09 00:07:24 -0400

    count the monkeys

---
+++ lvl8/countTheMonkeys!.ts
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/56f69d9f9400f508fb000ba7/solutions/javascript
+
+function monkeyCount(n: number) {
+  let arr = []
+  for(let i = 0; i < n; i++){
+    arr.push(i+1)
+  }
+  return arr
+}
