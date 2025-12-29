cc6ffd25a992f2d22256c58658253edf308fea22
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-12-29 12:48:32 -0500

    do i get a bonus

---
+++ lvl8/doIGetABonus.ts
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/typescript
+
+export class Kata {
+    public static bonusTime(salary:number, bonus:boolean):string {
+      if(bonus){
+        salary = salary * 10
+        return `\u00A3${salary}`
+      }
+      return `\u00A3${salary}`
+    }
+}
